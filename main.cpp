695ac3a1039dac810c8619a3125d64897af313f3
annasa164 <annasa164@gmail.com>
2024-03-24 12:00:43 +0400

    Change the function so that it calculates iteratively and outputs the first n Fibonacci numbers

--- main.cpp
+++ main.cpp
@@ -2,17 +2,25 @@
 
 using namespace std;
 
-int fib(int n){
-    if (n == 1)
-        return 0;
-    if (n == 2)
-        return 1;
-    return fib(n - 2) + fib(n - 1);
+void fib(int n){
+    int a1 = 0, a2 = 1, cur;
+    for (int i = 1; i <= n; i++){
+        if (i == 1)
+            cout << a1 << "\n";
+        else if (i == 2)
+            cout << a2 << "\n";
+        else{
+            cur = a1 + a2;
+            a1 = a2;
+            a2 = cur;
+            cout << a2 << "\n"; 
+        }
+    }
 }
 
 int main(){
     cout << "Hello, World!" << "\n";
     int n;
     cin >> n;
-    cout << fib(n) << "\n";
+    fib(n);
 }
